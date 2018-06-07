2766e04052c42c644d0deef0813f96885bebc60d
Matthew DEVILLE <mdeville@student.42.fr>
2018-06-07 15:41:01 +0200

    add array_iter

--- includes/array.h
+++ includes/array.h
@@ -6,7 +6,7 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 01:27:17 by mdeville          #+#    #+#             */
-/*   Updated: 2018/06/07 11:45:07 by mdeville         ###   ########.fr       */
+/*   Updated: 2018/06/07 15:25:42 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 
 # include "dlst.h"
 
-t_dlist		*ft_tab_to_dlst(void *tab, size_t nmemb, size_t size);
+t_dlist		*ft_arr_to_dlst(void *tab, size_t nmemb, size_t size);
 void		ft_qsort(
 					void *base,
 					size_t nmemb,
 					size_t size,
 					int (*compar)(const void *, const void *));
+void		ft_arriter(
+					void *base,
+					size_t nmemb,
+					size_t size,
+					void (*f)(void *));
 
 #endif
